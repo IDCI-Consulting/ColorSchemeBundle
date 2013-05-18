24cd55e262db4b420501c0e96a5eb3101a7739a8
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-05-18 23:48:35 +0200

    Add class bundle file

---
+++ IDCIColorSchemeBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @licence: GPL
+ *
+ */
+
+namespace IDCI\Bundle\ColorSchemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCIColorSchemeBundle extends Bundle
+{
+}
